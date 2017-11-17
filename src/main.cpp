d694e9c16a919c1ed1736e835504ff8b194cfb6a
msrks <mshrrks@gmail.com>
2017-11-17 19:39:15 +0900

    add collision avoidance(slow down if detect foward)

--- src/main.cpp
+++ src/main.cpp
@@ -161,7 +161,7 @@ vector<double> getXY(double s, double d, const vector<double> &maps_s, const vec
 }
 
 int lane = 1;
-double ref_vel = 49.5;
+double ref_vel = 0.0;
 
 int main() {
   uWS::Hub h;
@@ -238,6 +238,41 @@ int main() {
           	auto sensor_fusion = j[1]["sensor_fusion"];
 
             int prev_size = previous_path_x.size();
+
+            if (prev_size > 0)
+            {
+              car_s = end_path_s;
+            }
+
+            bool too_close = false;
+
+            for (int i = 0 ; i < sensor_fusion.size(); i++)
+            {
+              float d = sensor_fusion[i][6];
+              if (d < (2+4*lane+2) && d > (2+4*lane-2))
+              {
+                double vx = sensor_fusion[i][3];
+                double vy = sensor_fusion[i][4];
+                double check_speed = sqrt(vx*vx+vy*vy);
+                double check_car_s = sensor_fusion[i][5];
+
+                check_car_s += ((double)prev_size*0.02*check_speed);
+
+                if ( (check_car_s > car_s) && ((check_car_s-car_s) < 30))
+                {
+                  too_close = true;
+                }
+              }
+            }
+
+            if (too_close)
+            {
+              ref_vel -= .224;
+            }
+            else if (ref_vel < 49.5)
+            {
+              ref_vel += .224;
+            }
 
           	json msgJson;
 
